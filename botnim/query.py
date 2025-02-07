f12c8de487a28bdb3e15897dbcff9a95b52a2e0f
NoamC <noam.castel@zencity.io>
2025-02-07 10:56:23 +0200

    add a basic hybrid search

--- botnim/query.py
+++ botnim/query.py
@@ -32,6 +32,7 @@ class QueryClient:
             return {"name": f"{self.bot_name}_assistant"}
             
         with open(specs_dir) as f:
+<<<<<<< HEAD
             config = yaml.safe_load(f)
             return config
 
@@ -174,3 +175,48 @@ def format_mapping(mapping: Dict, indent: int = 0) -> str:
     
     return "\n".join(result)
 
+=======
+            return yaml.safe_load(f)
+        
+    def _initialize_vector_store(self) -> VectorStoreES:
+        """Initialize the vector store connection"""
+        return VectorStoreES(
+            config=self.config,  # Use the config from the QueryClient instance
+            es_host=os.getenv('ES_HOST', 'https://localhost:9200'),
+            es_username=os.getenv('ES_USERNAME', 'elastic'),
+            es_password=os.getenv('ES_PASSWORD'),
+            verify_certs=False  # TODO: change to True when in production to use the production index
+        )
+
+    def _build_search_query(self, query_text: str, embedding: List[float]) -> Dict[str, Any]:
+        """Build the hybrid search query"""
+
+        text_match = {
+            "multi_match": {            ## you can define here the fields you want to search in
+                "query": query_text,
+                "fields": ["content"],
+                "boost": 0.2,          ## you can define here the boost for the text match vs. vector match
+                "type": 'best_fields',  ## you can define here the type of search: cross_fields, bool, simple, phrase, phrase_prefix
+                "operator": 'or',       ## you can define here the operator: or, and 
+            }
+        }
+        
+        vector_match = {
+            "knn": {
+                "field": "vector",  # the field we want to search in
+                "query_vector": embedding,  # the embedding we want to search for
+                "k": 7,  # the number of results we want to get
+                "num_candidates": 20,  # the number of candidates we want to consider
+                "boost": 0.5  # the boost for the vector match
+            }
+        }
+        
+        return {
+            "bool": {
+                "should": [text_match, vector_match],
+                "minimum_should_match": 1,
+            }
+        }
+    
+    
+>>>>>>> ab635e1 (add a basic hybrid search)
