503e7355ffb1fe6a88590b8ce3d56ecb72eeea0d
NoamC <noam.castel@zencity.io>
2025-02-10 00:55:36 +0200

    add embedding constants to the right config file...

--- botnim/config.py
+++ botnim/config.py
@@ -14,3 +14,9 @@ logger = logging.getLogger(__name__)
 def get_logger(name: str) -> logging.Logger:
     """Get a logger instance for the given name"""
     return logging.getLogger(name)
+
+
+# Embedding model settings
+DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small'
+DEFAULT_EMBEDDING_SIZE = 1536
+DEFAULT_BATCH_SIZE = 50
