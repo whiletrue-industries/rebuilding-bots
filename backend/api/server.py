3f5d103e1f0402dc26bf3282d2db854c7cd16fda
Adam Kariv <adam.kariv@gmail.com>
2025-03-02 13:45:02 +0200

    Api Server Docker

---
+++ backend/api/server.py
@@ -0,0 +1,34 @@
+import dataclasses
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from typing import List, Dict, Any, Optional
+
+from botnim.query import run_query
+
+### A FastAPI server that serves the odds API
+# Exposes the following methods:
+# - search_datasets(query: str) -> List[Dict[str, str]]
+# - fetch_dataset(id: str) -> Optional[Dict[str, str]]
+# - fetch_resource(id: str) -> Optional[Dict[str, str]]
+# - query_db(resource_id: str, query: str) -> Optional[Dict[str, Any]]
+
+app = FastAPI(openapi_url=None)
+
+# Enable CORS:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to the specific origins you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/retrieve/{bot}/{context}")
+async def search_datasets_handler(bot: str, context: str, query: str, num_results: int=10, environment: str='production') -> List[Dict[str, Any]]:
+    results = run_query(query, environment, bot, context, num_results)
+    return results
+
+# Run the server with:
+# uvicorn server:app --reload
