19c3dcf7ab78a58d6d4d20807e4a672c352f72ba
NoamC <noam.castel@zencity.io>
2025-03-08 23:53:12 +0200

    Fix: Ensure tools are created for all contexts in Elasticsearch vector store

--- botnim/vector_store/vector_store_es.py
+++ botnim/vector_store/vector_store_es.py
@@ -264,12 +264,13 @@ class VectorStoreES(VectorStoreBase):
             return 0
 
     def update_tools(self, context_, vector_store):
-        """Create and return the search tool for this vector store"""
-        return self.create_search_tool(
+        """Create a search tool for this context and add it to self.tools"""
+        tool = self.create_search_tool(
             bot_name=self.config['slug'],
             context_name=context_['name'],
             environment='production' if self.production else 'staging'
         )
+        self.tools.append(tool)
 
     def update_tool_resources(self, context_, vector_store):
         # For Elasticsearch, we don't need to set tool_resources
