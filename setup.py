0e1a1f8fc11bc2e7ee5493a6647ba7b21a45095b
NoamC <noam.castel@zencity.io>
2025-07-18 12:57:19 +0300

    Fix formatting in setup.py by removing spaces around the equals sign in console_scripts entry point.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'botnim = botnim.cli:main',
+            'botnim=botnim.cli:main',
         ],
     },
     install_requires=[
